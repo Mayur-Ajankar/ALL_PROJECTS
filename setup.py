f07c0019a47db6f4e27e1cf0b9c23a3c836fe752
Mayur-Ajankar <mayurajankar151998@gmail>
2025-09-12 20:04:21 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -3,23 +3,26 @@ from typing import List
 
 
 HYPEN_E_DOT="-e ."
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path: str) -> List[str]:
     """
     This function will return the list of requirements
     """
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  # strip spaces & newlines
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
+    return requirements
+
+
 setup(
-    name="ML_and_GEN_AI_Projects"
-    version="0.0.1"
-    author="Mayur"
-    author_email="mayurajankar151998@gmail.com"
+    name="ML_and_GEN_AI_Projects",
+    version="0.0.1",
+    author="Mayur",
+    author_email="mayurajankar151998@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt")
 
